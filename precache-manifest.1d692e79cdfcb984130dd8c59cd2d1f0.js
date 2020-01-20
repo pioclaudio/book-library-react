107c5c53b089f6e5b70107a89af32be236e00b29
Pio <pioclaudio@gmail.com>
2020-01-19 23:05:06 -0500

    Updates

---
+++ precache-manifest.1d692e79cdfcb984130dd8c59cd2d1f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b870198070dd9cea7aa2e3fec641120",
+    "url": "/pioclaudio/book-library-react/index.html"
+  },
+  {
+    "revision": "c0f9cdfea2f4933d0169",
+    "url": "/pioclaudio/book-library-react/static/css/main.737ddcf1.chunk.css"
+  },
+  {
+    "revision": "ee2778e21c5e3b3c5086",
+    "url": "/pioclaudio/book-library-react/static/js/2.58484c7a.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/pioclaudio/book-library-react/static/js/2.58484c7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0f9cdfea2f4933d0169",
+    "url": "/pioclaudio/book-library-react/static/js/main.3787fb64.chunk.js"
+  },
+  {
+    "revision": "ce49dc1eb20e0875c0fd",
+    "url": "/pioclaudio/book-library-react/static/js/runtime-main.7cc21193.js"
+  }
+]);
